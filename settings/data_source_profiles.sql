207cbfab8254c09727936bb38646c08ba7783e29
Sean Cline <smcline06@gmail.com>
2017-04-11 01:17:32 -0700

    Profile SQL correction

--- settings/data_source_profiles.sql
+++ settings/data_source_profiles.sql
@@ -9,8 +9,8 @@ REPLACE INTO `%DB_NAME%`.`data_source_profiles_cf` (`data_source_profile_id`, `c
 REPLACE INTO `%DB_NAME%`.`data_source_profiles_cf` (`data_source_profile_id`, `consolidation_function_id`) VALUES('4', '3');
 REPLACE INTO `%DB_NAME%`.`data_source_profiles_cf` (`data_source_profile_id`, `consolidation_function_id`) VALUES('4', '4');
 
-REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`step`,`rows`) VALUES('9','4','Hourly (1 Minute Average)','1','1440');
-REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`step`,`rows`) VALUES('10','4','Daily (5 Minute Average)','5','600');
-REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`step`,`rows`) VALUES('11','4','Weekly (30 Minute Average)','30','700');
-REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`step`,`rows`) VALUES('12','4','Monthly (2 Hour Average)','120','775');
-REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`step`,`rows`) VALUES('13','4','Yearly (1 Day Average)','1440','797');
+REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`steps`,`rows`) VALUES('9','4','Hourly (1 Minute Average)','1','1440');
+REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`steps`,`rows`) VALUES('10','4','Daily (5 Minute Average)','5','600');
+REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`steps`,`rows`) VALUES('11','4','Weekly (30 Minute Average)','30','700');
+REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`steps`,`rows`) VALUES('12','4','Monthly (2 Hour Average)','120','775');
+REPLACE INTO `%DB_NAME%`.`data_source_profiles_rra` (`id`, `data_source_profile_id`,`name`,`steps`,`rows`) VALUES('13','4','Yearly (1 Day Average)','1440','797');
