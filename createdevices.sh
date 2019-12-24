58263c8d2be3a495925e7761109d738e216cba0f
Chris Lovett <clovett@mcnc.org>
2019-12-24 17:03:07 -0500

    More advanced Cacti population script

--- createdevices.sh
+++ createdevices.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-cd /cacti
+while getopts ":dgf:" opt; do
+  cd /cacti
+  case $opt in
+    f)
+      DEVLIST="$OPTARG"
+      ;;
+    d)
+      DEVICES=true
+      ;;
+    g)
+      GRAPHS=true
+      ;;
+    /?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
 
-while IFS="," read devname ipaddr comstring
-do
+if [[ $DEVICES = true ]]; then
+  while IFS="," read devname ipaddr comstring; do
     php cli/add_device.php --description=$devname --ip=$ipaddr --community=$comstring --template=8 --version=2 --site=0;
-done < /cacti/$1
+  done < /cacti/$DEVLIST
+fi
+
+if [[ $GRAPHS = true ]]; then
+  for i in $(php cli/add_graphs.php --list-hosts | awk '$3 ~ /8/ { print $1 }'); do
+    php cli/add_graphs.php --host-id="$i" --graph-type=ds --graph-template-id=13 --snmp-query-id=1 --snmp-query-type-id=49 --snmp-field=ifOperStatus --snmp-value=Up;
+  done
+fi
